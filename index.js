dc3574bc7574029215fc2baa9420346893f9b86e
jesse keane <me@jessekeane.me>
2014-02-16 20:10:12 -0800

    half time unit

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ function to_morse() {
   input_stream.write(document.getElementById('morse-in').value)
 }
 
-input_stream.pipe(morse()).pipe(norse()).pipe(vibrant())
+input_stream.pipe(morse()).pipe(norse(250)).pipe(vibrant())
