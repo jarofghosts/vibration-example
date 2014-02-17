94829f5c522a4f25bcfe2c8b04148379c83dd79d
jesse keane <me@jessekeane.me>
2014-02-16 20:05:57 -0800

    hrmd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var vibrant = require('vibrant'),
     through = require('through')
 
 var input_stream = through(examine)
+var inspect = through(examine)
 
 var link = document.getElementById('to-morse')
 
@@ -18,4 +19,4 @@ function to_morse() {
   input_stream.write(document.getElementById('morse-in').value)
 }
 
-input_stream.pipe(morse()).pipe(input_pipe).pipe(norse()).pipe(vibrant())
+input_stream.pipe(morse()).pipe(inspect).pipe(norse()).pipe(vibrant())
