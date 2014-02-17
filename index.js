9c883da63f39eb7e979a6dcc21f91fb80fb79c61
jesse keane <me@jessekeane.me>
2014-02-16 20:04:06 -0800

    wha

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ var vibrant = require('vibrant'),
     norse = require('norse')
     through = require('through')
 
-var input_stream = through()
+var input_stream = through(examine)
 
 var link = document.getElementById('to-morse')
 
 link.addEventListener('click', to_morse)
 
+function examine(data) {
+  console.log(data.toString())
+  this.queue(data)
+}
+
 function to_morse() {
-  input_stream.queue(document.getElementById('morse-in').value)
+  input_stream.write(document.getElementById('morse-in').value)
 }
 
 input_stream.pipe(morse()).pipe(norse()).pipe(vibrant())
